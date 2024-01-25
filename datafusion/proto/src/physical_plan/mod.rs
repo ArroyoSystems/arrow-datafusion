019d66fff45d7ef3e1ca1cafe485d5d9f9224edd
Jackson Newhouse <jacksonrnewhouse@gmail.com>
2024-01-25 10:48:22 -0800

    combine partial changes.

--- datafusion/proto/src/physical_plan/mod.rs
+++ datafusion/proto/src/physical_plan/mod.rs
@@ -1471,8 +1471,12 @@ impl AsExecutionPlan for PhysicalPlanNode {
                 AggregateMode::Single => protobuf::AggregateMode::Single,
                 AggregateMode::SinglePartitioned => {
                     protobuf::AggregateMode::SinglePartitioned
+                },
+                AggregateMode::CombinePartial => {
+                    unimplemented!()
                 }
             };
+
             let input_schema = exec.input_schema();
             let input = protobuf::PhysicalPlanNode::try_from_physical_plan(
                 exec.input().to_owned(),

ea2113ab006eeb43ffbbada39aada40357dace66
Burak Şen <buraksenb@gmail.com>
2024-11-13 19:56:46 +0300

    Convert `nth_value` builtIn function to User Defined Window Function (#13201)
    * refactored nth_value
    
    * continue
    
    * test
    
    * proto and rustlint
    
    * fix datatype
    
    * cont
    
    * cont
    
    * apply jcsherins early validation
    
    * docs
    
    * doc
    
    * Apply suggestions from code review
    
    Co-authored-by: Sherin Jacob <jacob@protoship.io>
    
    * passes lint but does not have tests
    
    * continue
    
    * Update roundtrip_physical_plan.rs
    
    * udwf, not udaf
    
    * fix bounded but not fixed roundtrip
    
    * added
    
    * Update datafusion/sqllogictest/test_files/errors.slt
    
    Co-authored-by: Sherin Jacob <jacob@protoship.io>
    
    ---------
    
    Co-authored-by: Sherin Jacob <jacob@protoship.io>
    Co-authored-by: berkaysynnada <berkay.sahin@synnada.ai>
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- datafusion/proto/src/generated/pbjson.rs
+++ datafusion/proto/src/generated/pbjson.rs
@@ -1665,9 +1665,6 @@ impl serde::Serialize for BuiltInWindowFunction {
     {
         let variant = match self {
             Self::Unspecified => "UNSPECIFIED",
-            Self::FirstValue => "FIRST_VALUE",
-            Self::LastValue => "LAST_VALUE",
-            Self::NthValue => "NTH_VALUE",
         };
         serializer.serialize_str(variant)
     }
@@ -1680,9 +1677,6 @@ impl<'de> serde::Deserialize<'de> for BuiltInWindowFunction {
     {
         const FIELDS: &[&str] = &[
             "UNSPECIFIED",
-            "FIRST_VALUE",
-            "LAST_VALUE",
-            "NTH_VALUE",
         ];
 
         struct GeneratedVisitor;
@@ -1724,9 +1718,6 @@ impl<'de> serde::Deserialize<'de> for BuiltInWindowFunction {
             {
                 match value {
                     "UNSPECIFIED" => Ok(BuiltInWindowFunction::Unspecified),
-                    "FIRST_VALUE" => Ok(BuiltInWindowFunction::FirstValue),
-                    "LAST_VALUE" => Ok(BuiltInWindowFunction::LastValue),
-                    "NTH_VALUE" => Ok(BuiltInWindowFunction::NthValue),
                     _ => Err(serde::de::Error::unknown_variant(value, FIELDS)),
                 }
             }
